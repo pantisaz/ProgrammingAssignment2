3ad65eae7b1743d73902f32b83ad90a7593bf8f8
pantisaz <santiago.l.paz@gmail.com>
2014-09-17 20:19:22 -0300

    Updated version with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,7 @@
+#This first function creates a cache matrix
+#A cache matrix stores data in a serparate environment so that future requests for that data can be served faster
+#To make a cache matrix we use the <<- operator to allocate different addresses in memory for the s and X variables
+
 makeCacheMatrix <- function(X = matrix()) {
   s <- NULL
   set <- function(Y) {
@@ -11,6 +15,11 @@ makeCacheMatrix <- function(X = matrix()) {
        setinverse = setinverse,
        getinverse = getinverse)
 }
+
+#The second function finds the inverse of the matrix created in the first function
+#It first checks to see if the inverse has already been calculated. 
+#If so, it gets the inverse from the cache and skips the computation. 
+#If not, it calculates the inverse of the matrix and sets the value of the inverse in the cache with the setinverse function
 
 cacheSolve <- function(X=matrix(), ...) {
   s <- X$getinverse()
